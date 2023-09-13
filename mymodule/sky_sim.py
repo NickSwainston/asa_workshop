eed2c9211c96f138d63cd60651c09b4c5c0811ca
Nick Swainston <nickaswainst@gmail.com>
2023-09-13 11:19:05 +1000

    Updates to argparse

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -3,27 +3,38 @@
 Determine Andromeda location in ra/dec degrees
 """
 
+import argparse
 from random import uniform
 from math import cos, sin, pi
+
+import mymodule
 
 
 NSRC = 1_000
 RA = '00:42:44.3'
 DEC = '41:16:09'
 
-
-def get_radec():
+def skysim_parser():
     """
-    Generate the ra/dec coordinates of Andromeda
-    in decimal degrees.
+    Configure the argparse for skysim
 
     Returns
     -------
-    ra : float
-        The RA, in degrees, for Andromeda
-    dec : float
-        The DEC, in degrees for Andromeda
+    parser : argparse.ArgumentParser
+        The parser for skysim.
     """
+    parser = argparse.ArgumentParser(prog='sky_sim', prefix_chars='-', description="Simulate a sky")
+    parser.add_argument('--version', action='version', version=f'%(prog)s {mymodule.__version__}')
+    parser.add_argument('--ra', dest = 'ra', type=float, default=None,
+                        help="Central ra (degrees) for the simulation location")
+    parser.add_argument('--dec', dest = 'dec', type=float, default=None,
+                        help="Central dec (degrees) for the simulation location")
+    parser.add_argument('--out', dest='out', type=str, default='catalog.csv',
+                        help='destination for the output catalog')
+    return parser
+
+
+def get_radec():
     # from wikipedia
     andromeda_ra = '00:42:44.3'
     andromeda_dec = '41:16:09'
@@ -72,13 +83,23 @@ def clip_to_radius(ra, dec, ras, decs):
 
 
 def main():
-    ra_deg, dec_deg = get_radec()
+    parser = skysim_parser()
+    options = parser.parse_args()
+
+    # if ra/dec are not supplied the use a default value
+    if None in [options.ra, options.dec]:
+        ra_deg, dec_deg = get_radec()
+    else:
+        ra_deg = options.ra
+        dec_deg = options.dec
+
     ras, decs = make_stars(ra_deg, dec_deg)
     ras, decs = clip_to_radius(ra_deg, dec_deg, ras, decs)
 
     # now write these to a csv file for use by my other program
-    with open('catalog.csv','w') as f:
+    with open(options.out,'w') as f:
         print("id,ra,dec", file=f)
         for i in range(len(ras)):
             print(f"{i:07d}, {ras[i]:12f}, {decs[i]:12f}", file=f)
+    print(f"Wrote {options.out}")
 
